7f1a7ac814514621a239351bc9a4b988483f6d59
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2022-03-10 20:34:21 +0545

    Add contents to .gitignore file

--- ecommerce/store/forms.py
+++ ecommerce/store/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from phonenumber_field.formfields import PhoneNumberField
 from .models import Customer
 
 class CustomUserCreationForm(UserCreationForm):
